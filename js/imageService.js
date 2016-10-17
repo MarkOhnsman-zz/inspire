a051f229fceb04bf94ac27509f466f1f97cd589b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2016-10-17 10:24:47 -0600

    security

--- js/imageService.js
+++ js/imageService.js
@@ -1,6 +1,6 @@
 function ImageService() {
 	var iService = this
-	var url = 'http://bcw-getter.herokuapp.com/?url=';
+	var url = 'https://bcw-getter.herokuapp.com/?url=';
 	var url2 = 'http://www.splashbase.co/api/v1/images/random'
 	var apiUrl = url + encodeURIComponent(url2);
 	// var image = ``
